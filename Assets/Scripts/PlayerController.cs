6c2caeded8b2aa9bacaf5b78fdfb63eb70fc0e73
fredericodgv <fredericodgv@gmail.com>
2025-06-26 20:44:09 -0300

    Remove pastas e arquivos denecessarios

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator animator;
-    public float xSpeed = 5.0f;
+    [Header("MOVEMENT SETTINGS")]
     public float jumpForce = 7.0f;
-    private Rigidbody2D rb;
-    private bool isGround = false;
-    public Transform isGroundCheck;
-    public LayerMask isGroundLayer;
-    private bool run;
-    private bool jump;
-    public Transform LevelClear;
-    public int life = 3;
-    public Transform Life1;
-    public Transform Life2;
-    public Transform Life3;
-    private AudioSource sound;
+    [SerializeField] private float xSpeed = 5.0f;
+    [SerializeField] private bool isGround = false;
+    [SerializeField] private Transform isGroundCheck;
+    [SerializeField] private LayerMask isGroundLayer;
+    [SerializeField] private bool run;
+    [SerializeField] private bool jump;
 
+    [Header("COMPONENT REFERENCES")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private AudioSource sound;
+
+    [Header("LIFE SYSTEM")]
+    [SerializeField] private int life = 3;
+    [SerializeField] private Transform life1;
+    [SerializeField] private Transform life2;
+    [SerializeField] private Transform life3;
+
+    [Header("LEVEL PROGRESS")]
+    [SerializeField] private Transform levelClear;
+    [SerializeField] private Transform gameOver;
 
 
 
@@ -27,25 +34,23 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        //GameOver.gameObject.SetActive(false);
-        //LevelClear.gameObject.SetActive(false);
+        gameOver.gameObject.SetActive(false);
+        levelClear.gameObject.SetActive(false);
         sound = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         jump = false;
-        
-        
 
-        //Primeiro verifica se o jogador está no chão;
+        //Verifica se o jogador está no chão;
         isGround = Physics2D.OverlapCircle(isGroundCheck.position, 0.5f, isGroundLayer);
 
         //Movimenta o jogador horizontalmente
-        float xInput = Input.GetAxis("Horizontal");
+        float xInput =Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * xSpeed, rb.velocity.y);
 
-        run = Mathf.Abs(xInput) > 0.3f;
+        run = Mathf.Abs(xInput) > 0.3;
 
 
         //Inverte a direção do sprite
@@ -65,7 +70,7 @@ public class PlayerController : MonoBehaviour
         //Verifica se o jogador está no chão e se o botão de pulo foi pressionado
         if (Input.GetKeyDown(KeyCode.W) && isGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(0, jumpForce);
             jump = true;
             jumpForce = -jumpForce; // Inverte a direção do pulo
             rb.gravityScale = -rb.gravityScale; // Inverte a gravidade do Rigidbody2D
@@ -100,6 +105,11 @@ public class PlayerController : MonoBehaviour
         {
             PlayerDeath();
         }
+
+        if (transform.position.x > 25)
+        {
+            levelClear.gameObject.SetActive(true);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -109,17 +119,17 @@ public class PlayerController : MonoBehaviour
             sound.Play();
             if(life == 3)
             {
-                Life3.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
                 life--;
             }
             else if(life == 2)
             {
-                Life2.gameObject.SetActive(false);
+                life2.gameObject.SetActive(false);
                 life--;
             }
             else if(life == 1)
             {
-                Life1.gameObject.SetActive(false);
+                life1.gameObject.SetActive(false);
                 life--;
                 PlayerDeath();
             }
@@ -130,7 +140,7 @@ public class PlayerController : MonoBehaviour
     void PlayerDeath()
     {
         Destroy(gameObject);
-        //GameOver.gameObject.SetActive(true);
+        gameOver.gameObject.SetActive(true);
     }
 
     public void DestroyPlayer()
